2af1e52414d6267b50990100322f0fca60acc827
SalyOkit <salym@okit.in>
2019-05-20 12:33:45 +0530

    Add api data and scoll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css'
 
 class App extends Component {
@@ -10,9 +10,15 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {return response.json();})
+        .then(users => {this.setState({robots:users})})
     }
 
     onSearchChange = (event) => {
@@ -24,14 +30,18 @@ class App extends Component {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
 
+        if(this.state.robots.length === 0){
+            return <h1 className='tc'>Loading...</h1>
+        }else{
         return (
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
-        )
+        )}
     }
 }
 
