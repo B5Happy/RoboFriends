c58c68719b624181a3f3c39ae49358c3bd6318cf
Saly <saly@juke4me.com>
2019-06-03 01:49:07 +0530

    Updates

---
+++ precache-manifest.745ba8cb0d1d7911140975511cd72cd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827825c6600e02743aee3045724de3de",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a0a04ebf375509bf2782",
+    "url": "/RoboFriends/static/css/main.008014be.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/RoboFriends/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "a0a04ebf375509bf2782",
+    "url": "/RoboFriends/static/js/main.9faa01f0.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
